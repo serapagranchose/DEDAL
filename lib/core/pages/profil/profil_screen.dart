127aa9844022cc95b24d82b6c88dc069eeb168a8
serapagranchose <seraphin.perrot@gmail.com>
2023-11-22 17:27:55 +0100

    Adding a basic UI for settings

--- lib/core/pages/profil/profil_screen.dart
+++ lib/core/pages/profil/profil_screen.dart
@@ -1,5 +1,8 @@
 import 'package:dedal/components/button/custom_button.dart';
 import 'package:dedal/components/layouts/register_layout.dart';
+import 'package:dedal/components/text_fields/main_text_field.dart';
+import 'package:dedal/constants/colors.dart';
+import 'package:dedal/components/button/icon_button.dart';
 import 'package:dedal/core/extensions/build_context_applocalisation_extention.dart';
 import 'package:dedal/core/extensions/get_it.dart';
 import 'package:dedal/core/extensions/string_extention.dart';
@@ -43,18 +46,82 @@ class ProfilScreen extends CubitScreen<ProfilCubit, CrudState> {
         child: Column(
           children: [
             if (state is CrudLoaded<User?>)
+              Column(
+                children: [
+                  /*
+                  MainTextFields(
+                    title: context.l18n!.loginEmail.capitalize(),
+                    onChanged: (String value) => (),
+                  ),
+                  MainTextFields(
+                    title: context.l18n!.loginEmail.capitalize(),
+                    onChanged: (String value) => (),
+                  ),
+                  */
+                  Container(height: 30),
+                  Text(
+                    '${state.data?.name}',
+                    textAlign: TextAlign.start,
+                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Container(height: 15),
+                  Text('${state.data?.email}'),
+                  Container(height: 30),
+                  // Modify Profile
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 90),
+                    child: Column(children: [
+                      CustomStringButton(
+                        backgroundColor: Colors.transparent,
+                        textColor: SharedColorPalette().mainDisable,
+                        border: Border.all(
+                          width: 2,
+                          color: SharedColorPalette().mainDisable,
+                        ),
+                        context: context,
+                        text: context.l18n!.profilModify.capitalize(),
+                        onTap: (_) async => (),
+                      ),
+                    ])
+                  ),
+                  Container(height: 15),
+                        ],
+              ),
               // To get User info just look in state.data.whateveryouwant
-              Text('email : ${state.data?.email}'),
-            CustomStringButton(
-              context: context,
-              text: context.l18n!.profilDeco.capitalize(),
-              onTap: (_) async => context.goNamed(Main.routeName),
-            ),
-            CustomStringButton(
-              context: context,
-              text: context.l18n!.profilDeletAccount.capitalize(),
-              onTap: (_) async => context.read<ProfilCubit>().unsubscribe(),
-            ),
+              Expanded(
+                child: Container(height: 15),
+              ),
+              Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Row(children: [
+                  // Delete account button
+                  Expanded(
+        child: CustomStringButton(
+                    border: Border.all(
+                      width: 2,
+                      color: SharedColorPalette().mainDisable,
+                    ),
+                    context: context,
+                    text: context.l18n!.profilDeletAccount.capitalize(),
+                    onTap: (_) async => context.read<ProfilCubit>().unsubscribe(),
+                  ),
+                  ),
+                  Container(width: 30),
+                  // Log out button
+                  Expanded(
+        child: CustomStringButton(
+                    backgroundColor: Colors.transparent,
+                    textColor: SharedColorPalette().mainDisable,
+                    border: Border.all(
+                      width: 2,
+                      color: SharedColorPalette().mainDisable,
+                    ),
+                    context: context,
+                    text: context.l18n!.profilDeco.capitalize(),
+                    onTap: (_) async => context.goNamed(Main.routeName),
+                  ),
+                )])
+              ),
           ],
         ),
       );
